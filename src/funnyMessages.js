020b8a97dc635f176b7819d85cdea9d68b0edb9e
jahidanowar <milon5143@gmail.com>
2021-06-21 09:20:17 +0530

    update: messages

--- src/funnyMessages.js
+++ src/funnyMessages.js
@@ -5,4 +5,5 @@ module.exports = [
   "Are kitna aalsi ho tum. Website pe check karo na 😤",
   "Yelo aur firse mat puchna tang hochuka hu mai 😠",
   "Ek bat batau kitna time lagta hai website pe check karne mai? Nehi Sahab ko mujhse hi puch na hai 😒",
+  "Ha bhai sab mai hi bata du?😠 Last time de raha hu firse mat mangna."
 ];
